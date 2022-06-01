7bf512b7a60b9ca1817988f4b9e34ad4b346959f
vinhkute24 <thitran2vinh93@gmail.com>
2022-06-01 13:03:09 +0700

    update

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ if(process.env.NODE_ENV === 'production'){
 
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>{
-    console.log('Server is running on port', PORT)
-})
+
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 module.exports = app;
